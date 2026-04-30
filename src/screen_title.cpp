f1e13dbca852041ba21d85c2fff6fdd2814346e9
Trace <christra011@alpinesd.org>
2026-04-30 08:47:11 -0600

    More Title Screen

--- src/screen_title.cpp
+++ src/screen_title.cpp
@@ -29,8 +29,11 @@
 //----------------------------------------------------------------------------------
 static int framesCounter = 0;
 static int finishScreen = 0;
+static int selection = 1;
 bool loadGame = false;
 bool loadEditor = false;
+static Rectangle loadGameRec = {200, screenHeight/2.0f, 100, 50};
+static Rectangle loadEditorRec = {400, screenHeight/2.0f, 100, 50};
 static Vector2 selectionPos = {260, (screenHeight/2.0f)-20};
 //----------------------------------------------------------------------------------
 // Title Screen Functions Definition
@@ -48,17 +51,29 @@ void InitTitleScreen(void)
 void UpdateTitleScreen(void)
 {
     // TODO: Update TITLE screen variables here!
-    if (IsKeyPressed(KEY_RIGHT || KEY_D))
+    if (IsKeyPressed(KEY_RIGHT) || IsKeyPressed(KEY_D) || CheckCollisionPointRec(GetMousePosition(), loadEditorRec))
     {
         selectionPos.x = 460;
+        selection = 2; //Editor
     }
-    else if (IsKeyPressed(KEY_LEFT|| KEY_A))
+    else if (IsKeyPressed(KEY_LEFT) || IsKeyPressed(KEY_A) || CheckCollisionPointRec(GetMousePosition(), loadGameRec))
     {
         selectionPos.x = 260;
+        selection = 1; //Gameplay
     }
-    
     // Press enter or tap to change to GAMEPLAY screen
-    if (IsKeyPressed(KEY_ENTER) || loadGame)
+    if (IsKeyPressed(KEY_ENTER))
+    {
+        if (selection == 1)
+        {
+            finishScreen = 2;
+        }
+        else if (selection == 2)
+        {
+            finishScreen = 3;
+        }
+    }
+    if (loadGame)
     {
         //finishScreen = 1;   // OPTIONS
         finishScreen = 2;   // GAMEPLAY
@@ -81,7 +96,7 @@ void DrawTitleScreen(void)
     DrawText("Created using Raylib", 20, 95, 20, BLACK);
     // DrawTriangle(Vector2 {260, (screenHeight/2.0f)-20}, Vector2 {240, (screenHeight/2.0f)-20}, Vector2 {250, (screenHeight/2.0f)-10}, BLACK);
     DrawTriangle(selectionPos, Vector2Subtract(selectionPos, Vector2 {20, 0}), Vector2Add(selectionPos, Vector2 {-10, 10}), BLACK);
-    loadGame = GuiButton({200, screenHeight/2.0f, 100, 50}, "START GAME");
+    loadGame = GuiButton(loadGameRec, "START GAME");
     loadEditor = GuiButton({400, screenHeight/2.0f, 100, 50}, "STAGE EDITOR");
 }
 
